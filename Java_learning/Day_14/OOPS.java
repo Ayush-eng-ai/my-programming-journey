811b16f1415a28d0760d060db855a517eaef2138
Ayush <25f1002575@ds.study.iitm.ac.in>
2026-05-25 17:45:50 +0530

    Restored and re-uploaded complete programming journey repository after repository issue

--- Java_learning/Day_14/OOPS.java
+++ Java_learning/Day_14/OOPS.java
@@ -1,3 +1,5 @@
+
+
 public class OOPS {
 
     public static void main(String[] args) {
@@ -98,6 +100,30 @@ class OOPS1 {
 
         System.out.println("Student Name: " + s2.name);
         System.out.println("Student Roll: " + s2.roll);  
+
+
+        Student1 s3 = new Student1();
+        s3.name = "Rohit";
+        s3.roll = 102;
+        s3.password = "rohit123";
+        s3.marks[0] = 85;
+        s3.marks[1] = 90;
+        s3.marks[2] = 95;
+        for(int i = 0; i < 3; i++) {
+            System.out.println(s3.marks[i]);
+        }
+
+        Student1 s4 = new Student1(s3);
+        s4.password = "ayush123";
+        s3.marks[2] = 100;
+        System.out.println("blow up copy numbers:");
+        for(int i = 0; i < 3; i++) {
+            
+            System.out.println( + s3.marks[i]);
+        }
+       
+        System.out.println("Student Name: " + s3.name);
+        System.out.println("Student Roll: " + s3.roll);
     }
 
     static class Student {
@@ -115,6 +141,243 @@ class OOPS1 {
             this.roll = roll;
         }
        
+    }
+
+
+    //  uses copy constructor
+    static class Student1 {
+        String name;
+        int roll;
+        String password;
+        int marks[];
+
+        // Shallow copy constructor
+        // Student1(Student1 s3) {
+        //     marks = new int[3];
+        //     this.name = s3.name;
+        //     this.roll = s3.roll;
+        //     this.marks = s3.marks;
+
+        // }
+
+
+        // Deep copy constructor
+        Student1(Student1 s3) {
+            this.name = s3.name;
+            this.roll = s3.roll;
+            this.marks = s3.marks.clone();
+        }
+        Student1() {
+            marks = new int[3];
+            System.out.println("This is a default constructor");
+        }
+        Student1(String name) {
+            marks = new int[3];
+            this.name = name;
+        }
+        Student1(int roll) {
+            marks = new int[3];
+            this.roll = roll;
+        }
+
+
+
+    }
+
+        
+}
+
+// Inheritance:
+//  1. Single Inheritance: A class inherits from a single parent class.
+//  2. Multilevel Inheritance: A class inherits from a parent class, which
+// 3. Hierarchical Inheritance: Multiple classes inherit from a single parent class.
+// 4. Hybrid Inheritance: A combination of two or more types of inheritance. (Not supported in Java)
+class OOPS2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // Fish shark = new Fish();
+        // shark.color = "Grey";
+        // shark.fins = 8;
+        // System.out.println("Fish Color: " + shark.color);
+        // System.out.println("Fish Fins: " + shark.fins);
+        // shark.eat();
+        // shark.sleep();
+        // shark.breath();
+        // shark.swim();
+
+        // Dog dobby = new Dog();
+        // dobby.color = "Brown";
+        // dobby.legs = 4;
+        // System.out.println("Dog Color: " + dobby.color);
+        // System.out.println("Dog Legs: " + dobby.legs);
+        // dobby.breed = "Labrador";
+        // System.out.println("Dog Breed: " + dobby.breed);
+    }
+// base class or parent class or super class
+    static class Animal{
+        String color;
+        void eat() {
+            System.out.println("Eating...");
+        }
+        void sleep() {
+            System.out.println("Sleeping...");
+        }
+        void breath() {
+            System.out.println("Breathing...");
+        }
+
+    }
+
+// Hierarchical Inheritance
+    static class Mammals extends Animal {
+        void walk() {
+            System.out.println("Walking...");
+        }
+
+    }
+
+    static class Fish extends Animal {
+        int fins;
+        void swim() {
+            System.out.println("Swimming...");
+        }
+
+    }
+
+    static class Bird extends Animal {
+        void fly() {
+            System.out.println("Flying...");
+        }
+    }       
+
+
+// Multiple Inheritance is not supported in Java, but we can achieve it through interfaces.
+    // static class Mammals extends Animal {
+    //     int legs;
+    //     void walk() {
+    //         System.out.println("Walking...");
+    //     }
+    // }
+
+    // static class Dog extends Mammals {
+    //     String breed;
+    //     void bark() {
+    //         System.out.println("Barking...");
+    //     }
+    // }
+
+// Single Inheritance
+// derived class or child class or sub class
+    // static class Fish extends Animal {
+    //     int fins;
+    //     void swim() {
+    //         System.out.println("Swimming...");
+    //     }
+
+    // }
+}
+
+
+
+// Polymorphism:
+// 1. Compile-time Polymorphism (Method Overloading): When multiple methods have the
+// same name but different parameters, it is called method overloading. The method to be called is determined at compile time.
+// 2. Runtime Polymorphism (Method Overriding): When a subclass provides a specific implementation of a method that is already defined in its superclass, it is called method overriding. The method to be called is determined at runtime based on the object type.
+
+
+class OOPS3 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Calculator calc = new Calculator();
+        System.out.println("Sum of 2 and 3: " + calc.add(2, 3));
+        System.out.println("Sum of 2.5 and 3.5: " + calc.add(2.5f, 3.5f));
+        System.out.println("Sum of 1, 2 and 3: " + calc     .add(1, 2, 3)); 
+
+        Deer deer = new Deer();
+        deer.eat();
+
+    }   
+    static class Calculator {
+        // Method Overloading
+        int add(int a, int b) {
+            return a + b;
+        }
+
+        float add(float a, float b) {
+            return (a + b);
+        }
+        int add(int a, int b, int c) {
+            return a + b + c;
+        }
+    }
+
+    // Method Overriding
+    static class Animal {
+        void eat () {
+            System.out.println("Eats Anything...");
+        }
+    }
+
+    static class Deer extends Animal{
+        void eat() {
+            System.out.println("Eats Grass...");
+        }
+    }
+
+
+}
+
+// Abstraction: Abstraction is the process of hiding the implementation details and showing only the functionality to the user. In Java, abstraction can be achieved using abstract classes and interfaces.
+class OOPS4 {
+    public static void main (String args[]) {
+        System.out.println("Hello World!");
+
+        Horse horse = new Horse();
+        horse.eat();
+        horse.walk();
+        System.out.println(horse.color);
+
+
+        Chicken chicken = new Chicken();
+        chicken.eat();
+        chicken.walk();
+    }
+
+
+
+}
+
+abstract class Animal{
+    public String color;
+
+    Animal() {
+       color = "Brown";
+    }
+    void eat() {
+        System.out.println("Eats Anything...");
+    }
+
+    abstract void walk();
+
+}
+
+class Horse extends Animal {
+    void changeColor() {
+        color = "Black";
+    }
+    void walk() {
+        System.out.println("Walks on 4 legs...");
+    }
+}
+
+class Chicken extends Animal {
+    void changeColor() {
+        color = "White";
+    }
+    void walk() {
+        System.out.println("Walks on 2 legs...");
     }
 }
    
