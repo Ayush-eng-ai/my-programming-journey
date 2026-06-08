c9c0ab1ae6d56c98fdddb3c8205589435bc02013
Ayush <ayushrajput8404@gmail.com>
2026-06-08 15:16:43 +0530

    I did write the code  and solve the question

--- Java_learning/Day_16/RecursionBasic_part2.java
+++ Java_learning/Day_16/RecursionBasic_part2.java
@@ -34,7 +34,26 @@ public class RecursionBasic_part2 {
         }   
     }
 
+// Problem 3: Friends Pairing Problem:
+		// Given n friends, each one can remain single or can be paired up with some other friend. Each friend can be paired only once. Find out the total 	number of ways in which friends can remain single or can be paired up.
 
+	public static int friendsPairing(int n) {
+		if(n == 1 || n == 2) {
+			return n;
+		}
+
+		//choice
+		//single
+		int fnm1 = friendsPairing(n-1);
+		
+		//Pair
+		int fnm2 = friendsPairing(n-2);
+		int pairingWays = (n - 1) * fnm2;
+		
+		//total Ways
+		int totWays = fnm1 + pairingWays;
+		return totWays;
+	}
 
     public static void main(String args[]) {
     // Test the tiling problem
@@ -44,6 +63,9 @@ public class RecursionBasic_part2 {
     // Test the remove duplicates problem
         String inputString = "aabbcc";
         System.out.println("String after removing duplicates: " + removeDuplicates(inputString));   
+	
+	//Friends Pairing
+	System.out.println(friendsPairing(3));
         
     }
 }
